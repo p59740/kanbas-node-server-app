bbd7b30ccd63356bfce9b0149f117ce1c7f4c3f9
p59740 <peng.yue2@northeatsern.edu>
2023-11-17 06:30:57 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from 'express'
+import Hello from "./hello.js"
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from './assignments/routes.js';
+import cors from "cors";
+// const express = require('express')
+const app = express()
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.use(cors());
+app.use(express.json());
+AssignmentRoutes(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
