f0780b9a5d604662a86df9eb916a01c269e756a1
p59740 <121592993+p59740@users.noreply.github.com>
2023-12-04 16:28:01 -0800

    a6 mongodb

--- app.js
+++ app.js
@@ -6,18 +6,50 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
 import cors from "cors";
+import mongoose from "mongoose";  // load the mongoose library
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-cs5610-fa23";
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas-cs5610-fa23"); // connect to the kanbas database
 // const express = require('express')
 const app = express()
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
 // app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-app.use(cors());
+app.use(cors({
+    credentials: true, // support cookies
+    // origin: "http://localhost:3000",  // restrict cross origin resource sharing to the react application
+    origin: process.env.FRONTEND_URL
+}));
+
 // app.use(
 //     cors({
 //       credentials: true,
 //       origin: process.env.FRONTEND_URL
 //     })
 //   );  
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+
+app.use(session(sessionOptions));
+  
+
+  
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
